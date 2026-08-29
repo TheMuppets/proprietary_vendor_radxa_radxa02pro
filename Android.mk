778ec3b2abe4c73f9cd25f55f8fa2b7147f1f056
Bruno Martins <bgcngm@gmail.com>
2026-08-29 23:45:56 +0100

    radxa02pro: Populate vendor with firmware
    These files were imported from the device tree.

---
+++ Android.mk
@@ -0,0 +1,14 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),radxa02pro)
+
+$(call add-radio-file-sha1-checked,radio/bootloader-console.img,e884c98e9205a4fe95491f3d54aaaf16ec50ed33)
+$(call add-radio-file-sha1-checked,radio/bootloader-recovery.img,b3f31820225b164fd7e5679cf764916d1bb935d2)
+$(call add-radio-file-sha1-checked,radio/bootloader.img,50c9c15b35f8561750fad2fef1275ab6af6cbe6e)
+$(call add-radio-file-sha1-checked,radio/misc.img,eb37860b2030f3d29ab61af22b96bae52bc7d42e)
+
+endif
