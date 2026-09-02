d5b5f0d819d42f591ff0dec9ffa9a8e4ef0454fd
Nolen Johnson <johnsonnolen@gmail.com>
2026-09-02 15:10:53 -0400

    radxa02pro: Update bootloader image prebuilts
    * Correct storeboot + update FIP toolchain to match.

--- Android.mk
+++ Android.mk
@@ -6,9 +6,9 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),radxa02pro)
 
-$(call add-radio-file-sha1-checked,radio/bootloader-console.img,34baf441da3cfd644a6663c2c3f507c40ecb22ec)
-$(call add-radio-file-sha1-checked,radio/bootloader-recovery.img,7ef2773b2928317122476c3a24009893086ac4bc)
-$(call add-radio-file-sha1-checked,radio/bootloader.img,1a123adcdd5d20d0f6487a856d4f95db270b0957)
+$(call add-radio-file-sha1-checked,radio/bootloader-console.img,0c586768e5a8d970d2e46bb6a7c9df12e36f668e)
+$(call add-radio-file-sha1-checked,radio/bootloader-recovery.img,a4a15abc2f3c764f57cb1773bc31a1ce97a9fed2)
+$(call add-radio-file-sha1-checked,radio/bootloader.img,209443005b5bbd47937499f44b0261ef1dfb4c8c)
 $(call add-radio-file-sha1-checked,radio/misc.img,eb37860b2030f3d29ab61af22b96bae52bc7d42e)
 
 endif
