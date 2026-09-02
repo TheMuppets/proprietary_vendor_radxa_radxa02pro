bc797dc8f94b8c06938646b91dd3c1507718f999
Nolen Johnson <johnsonnolen@gmail.com>
2026-09-02 11:54:20 -0400

    radxa02pro: Update bootloader image prebuilts
    * g12b support + use public toolchains.

--- Android.mk
+++ Android.mk
@@ -6,9 +6,9 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),radxa02pro)
 
-$(call add-radio-file-sha1-checked,radio/bootloader-console.img,e884c98e9205a4fe95491f3d54aaaf16ec50ed33)
-$(call add-radio-file-sha1-checked,radio/bootloader-recovery.img,b3f31820225b164fd7e5679cf764916d1bb935d2)
-$(call add-radio-file-sha1-checked,radio/bootloader.img,50c9c15b35f8561750fad2fef1275ab6af6cbe6e)
+$(call add-radio-file-sha1-checked,radio/bootloader-console.img,34baf441da3cfd644a6663c2c3f507c40ecb22ec)
+$(call add-radio-file-sha1-checked,radio/bootloader-recovery.img,7ef2773b2928317122476c3a24009893086ac4bc)
+$(call add-radio-file-sha1-checked,radio/bootloader.img,1a123adcdd5d20d0f6487a856d4f95db270b0957)
 $(call add-radio-file-sha1-checked,radio/misc.img,eb37860b2030f3d29ab61af22b96bae52bc7d42e)
 
 endif
