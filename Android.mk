ef0cd09e8b61549f3fc3cf9921a455e5c66cc091
Nolen Johnson <johnsonnolen@gmail.com>
2026-09-04 16:21:33 -0400

    radxa02pro: Update bootloader image prebuilts

--- Android.mk
+++ Android.mk
@@ -6,9 +6,9 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),radxa02pro)
 
-$(call add-radio-file-sha1-checked,radio/bootloader-console.img,0c586768e5a8d970d2e46bb6a7c9df12e36f668e)
-$(call add-radio-file-sha1-checked,radio/bootloader-recovery.img,a4a15abc2f3c764f57cb1773bc31a1ce97a9fed2)
-$(call add-radio-file-sha1-checked,radio/bootloader.img,209443005b5bbd47937499f44b0261ef1dfb4c8c)
+$(call add-radio-file-sha1-checked,radio/bootloader-console.img,3d51e90edd136d4387f5dc8486a8cfa42f58d354)
+$(call add-radio-file-sha1-checked,radio/bootloader-recovery.img,139f099cd2ac9ae1b178a1c69cd75c61e0c129bf)
+$(call add-radio-file-sha1-checked,radio/bootloader.img,4efab256b9dcde22857dd870b3bbe8efce0142b5)
 $(call add-radio-file-sha1-checked,radio/misc.img,eb37860b2030f3d29ab61af22b96bae52bc7d42e)
 
 endif
