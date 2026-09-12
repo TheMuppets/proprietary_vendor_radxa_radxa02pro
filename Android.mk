fd80bc9b3ae1a76a342f7f4251c9312e914848d6
Bruno Martins <bgcngm@gmail.com>
2026-09-12 01:35:05 +0100

    radxa02pro: Remove console and recovery BL images
    Change-Id: Ic517b53894669dae7ca39fef3904862afbb4866f

--- Android.mk
+++ Android.mk
@@ -6,8 +6,6 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),radxa02pro)
 
-$(call add-radio-file-sha1-checked,radio/bootloader-console.img,0c586768e5a8d970d2e46bb6a7c9df12e36f668e)
-$(call add-radio-file-sha1-checked,radio/bootloader-recovery.img,a4a15abc2f3c764f57cb1773bc31a1ce97a9fed2)
 $(call add-radio-file-sha1-checked,radio/bootloader.img,209443005b5bbd47937499f44b0261ef1dfb4c8c)
 $(call add-radio-file-sha1-checked,radio/misc.img,eb37860b2030f3d29ab61af22b96bae52bc7d42e)
 
